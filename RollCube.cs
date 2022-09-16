9b50e008e58d129c0102ce969b3d56faa0681f4a
John Lo <jdl283@cornell.edu>
2022-09-16 15:29:08 -0400

    Create RollCube.cs

---
+++ RollCube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollCube : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float force = 300;
+    public float rotation = 600;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Random.Range(100, 500)
+            rb.AddForce(new Vector3(0, force, 0));
+            rb.AddTorque(new Vector3(Random.Range(0,rotation), Random.Range(0, rotation), Random.Range(0,rotation)));
+        }
+    }
+}
